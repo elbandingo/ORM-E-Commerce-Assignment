9c047f3352cccdebc8acb71134877d46dda6f6f6
el bandingo <elbandingo@protonmail.com>
2022-01-23 15:47:46 -0330

    added get by ID, and also POST a new category, both tested and working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,10 +25,38 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'stock', 'price', 'category_id']
+      }
+    ]
+  }).then(categoryData => {
+    if(!categoryData) {
+      res.status(400).json({message: `no category found on ${req.params.id}`});
+      return;
+    }
+    res.json(categoryData);
+  }).catch(error => {
+    console.log(error);
+    res.status(500).json(error);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  }).then(categoryData => res.json(categoryData))
+  .catch(error => {
+    console.log(error);
+    res.status(500).json(error);
+  })
 });
 
 router.put('/:id', (req, res) => {
