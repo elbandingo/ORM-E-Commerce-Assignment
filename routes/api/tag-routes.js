8703e6f2da1ae4d9a73b71069b74007d9859ec21
el bandingo <elbandingo@protonmail.com>
2022-01-23 17:30:30 -0330

    working version, ready for submitting, all routes tested

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -49,14 +49,49 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  }).then(tagData => res.json(tagData))
+  .catch(error => {
+    console.log(error);
+    res.status(500).json(error);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(tagData => {
+    if(!tagData[0]) {
+      res.status(404).json({message: `There is no tag with ID ${req.params.id}`});
+      return;
+    }
+    res.json(tagData)
+  }).catch(error => {
+    console.log(error);
+    res.status(500).json(error);
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(tagData => {
+    if (!tagData) {
+      res.status(404).json({message: `There is no tag with ID ${req.params.id}`});
+      return;
+    }
+    res.json(tagData);
+  }).catch(error => {
+    console.log(error);
+    res.status(500).json(error)
+  })
 });
 
 module.exports = router;
